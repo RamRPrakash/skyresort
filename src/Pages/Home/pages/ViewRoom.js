1ee3feb249279150b7ea3aaf6101132c417b678a
RamRPrakash <prakashparthi1996@gmail.com>
2019-12-20 09:04:16 +0530

    Demo changes

--- src/Pages/Home/pages/ViewRoom.js
+++ src/Pages/Home/pages/ViewRoom.js
@@ -3,25 +3,24 @@ import { Paper, Modal } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import ModelView from "../../Model/ModelView";
 
-
 class ViewRoom extends Component {
-  constructor(props){
-  super(props);
-  this.state = {
-    ourRooms : false
+  constructor(props) {
+    super(props);
+    this.state = {
+      ourRooms: false
+    };
   }
-  
-  }
-  ourRooms=()=>{
+  ourRooms = () => {
     this.setState({
-      ourRooms : true
-    })
-  }
-  CloseRoom=()=>{
+      ourRooms: true , 
+     
+    });
+  };
+  CloseRoom = () => {
     this.setState({
-      ourRooms : false
-    })
-  }
+      ourRooms: false
+    });
+  };
   render() {
     return (
       <Paper style={{ height: "250px" }}>
@@ -29,16 +28,25 @@ class ViewRoom extends Component {
           style={{
             margin: "50px",
             height: "200px",
-            backgroundImage: `url(1.jpg)` ,
-            backgroundPosition : "center center" ,
-            backgroundSize : "cover" , 
-            position : 'relative' ,
-            color : 'white' ,
-            zIndex : 1
+            backgroundImage: `url(1.jpg)`,
+            backgroundPosition: "center center",
+            backgroundSize: "cover",
+            position: "relative",
+            color: "white",
+            zIndex: 1
           }}
         >
-        <div style={{ position : 'absolute' , top : 0 , bottom : 0 , left : 0 , right : 0 , backgroundColor : "#00000077" , zIndex : -1 }}>
-          </div>
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              backgroundColor: "#00000077",
+              zIndex: -1
+            }}
+          />
           <div>
             <div style={{ textAlign: "center" }}>
               <h3>Luxury Room</h3>
@@ -49,19 +57,66 @@ class ViewRoom extends Component {
             </div>
             <br />
             <div style={{ textAlign: "center" }}>
-              <Button variant="contained" style={{backgroundColor : 'orange'}} color="succes" onClick={this.ourRooms}>
+              <Button
+                variant="contained"
+                style={{ backgroundColor: "orange" }}
+                color="succes"
+                onClick={this.ourRooms}
+              >
                 Our Rooms
               </Button>
             </div>
           </div>
         </Paper>
-<ModelView open={this.state.ourRooms} >
-<div style={{width : '100px' , backgroundColor : 'white' , height : '100px'}}>
-<Button variant="contained" style={{backgroundColor : 'orange'}} color="succes" onClick={this.CloseRoom}>
-                Close
-              </Button>
-  </div>
-  </ModelView>
+        
+        <ModelView open={this.state.ourRooms}>
+            {/* this.state.ourRooms */}
+            <div style={{width : '100%' }}>
+              {/* <div style={{width : '100px' , 
+              height : '100px' , 
+              backgroundColor :'red' ,
+              margin : '10px' , 
+              float : 'left'
+              }}>
+                </div>
+                <div style={{width : '200px' , 
+              height : '100px' , 
+              backgroundColor :'red' ,
+              margin : '10px'}}>
+                </div> */}
+            <div
+                style={{
+                  backgroundImage: "url(1.jpg)",
+                  backgroundSize: "cover",
+                  height: "200px",
+                  width: "200px",
+                 
+                  float :'left'
+                }}
+              />
+              {/* <div
+                style={{
+                  backgroundImage: "url(1.jpg)",
+                  backgroundSize: "cover",
+                  height: "200px",
+                  width: "200px" ,
+                
+                  
+                }}
+              /> */}
+
+            
+            <Button
+              variant="contained"
+              style={{ backgroundColor: "orange" }}
+              color="succes"
+              onClick={this.CloseRoom}
+            >
+              Close
+            </Button>
+          </div>
+     
+        </ModelView>
       </Paper>
     );
   }
--- src/Pages/Model/ModelView.js
+++ src/Pages/Model/ModelView.js
@@ -1,18 +1,21 @@
-import React , {Component} from 'react'
+import React, { Component } from "react";
 
-class ModelView extends Component{
-  render(){
-    return(
-      <div style={{ display: (this.props.open) ? 'block' : 'none'  , 
-      position: 'fixed' , 
-      zIndex: 100 ,
-      bottom : 0 , 
-      top : 0 , 
-      right : 0 , 
-      left : 0 , 
-      backgroundColor: 'rgba(0,0,0,0.4)',
-      overflow : 'hidden' 
-      }} >
+class ModelView extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          display: this.props.open ? "block" : "none",
+          position: "fixed",
+          zIndex: 100,
+          bottom: 0,
+          top: 0,
+          right: 0,
+          left: 0,
+          backgroundColor: "rgba(0,0,0,0.4)",
+          overflow: "hidden"
+        }}
+      >
         {/* <div style={{
                         borderBottom: '1px solid #D5D5D5',
                         height: 50,
@@ -29,22 +32,24 @@ class ModelView extends Component{
                         <div className="model_title">{this.props.title}</div>
 
       </div> */}
-      <div style={{
-                        display: 'block',
-                        backgroundColor: '#fff',
-                        width: '80%' ,
-                        height: '60%',
-                        overflow: 'auto',
-                        margin : '10%' ,
-                        borderBottomLeftRadius: 5,
-                        borderBottomRightRadius: 5,
-                        alignItems:'center',
-                        justifyContent:'center'
-                    }}>
-                        {this.props.children}
-                    </div>
+        <div
+          style={{
+            display: "block",
+            backgroundColor: "#fff",
+            width: "80%",
+            height: "60%",
+            overflow: "auto",
+            margin: "10%",
+            borderBottomLeftRadius: 5,
+            borderBottomRightRadius: 5,
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          {this.props.children}
         </div>
-    )
+      </div>
+    );
   }
 }
-export default ModelView
+export default ModelView;
